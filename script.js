58057bc8447c051f0256c8f91b7aa739d7555532
nguyt1 <nguyt1@users.noreply.github.com>
2019-09-26 16:38:03 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-$.getJSON('ibc_ip_and_port_ranges.json',function(data){
-console.log(data);
-var output = '<ul>';  
-$.each(data, function(key,val){
-  output += '<li>'+ val.serviceName + " " + val.urls + '</li>' + " " + val.tcpPorts + '<li>';
-});
-output += '</ul>';
-$('#update').html(output);
+$(document).ready(function() {
+            $("#driver").click(function(event){
+               $.getJSON('ibc_ip_and_port_ranges.json',function(data){
+                 console.log(data);
+                 var output = '<ul>';  
+                 $.each(data, function(key,val){
+                  output += '<li>'+ val.serviceName + " " + val.urls + '</li>' + " " + val.tcpPorts + '<li>';
+                 });
+                 output += '</ul>';
+                 $('#update').html(output);
+               });
+            });
 });
