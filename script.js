4009bf775dcc3be290dc9345fbc043ce625d8977
nguyt1 <tin.nguyen@sasktel.com>
2019-09-26 15:17:22 -0600

    Test jquery getJSON

---
+++ script.js
@@ -0,0 +1,9 @@
+$.getJSON('ibc_ip_and_port_ranges.json',function(data){
+console.log(data);
+var output = '<ul>';  
+$.each(data, function(key,val){
+  output += '<li>'+ val.serviceName + " " + val.urls + '</li>' + " " + val.tcpPorts + '<li>';
+});
+output += '</ul>';
+$('#update').html(output);
+});
