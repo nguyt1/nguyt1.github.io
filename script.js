c3ca6d26b4ef47fc72857af97654f4129b79d6d6
nguyt1 <nguyt1@users.noreply.github.com>
2019-09-30 16:18:15 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
 var output;
-var json_file_name = 'ibc_ip_and_port_ranges.json';
-
-function read_json_file()
-{
-            $.getJSON('ibc_ip_and_port_ranges.json',function(data){});
-}
+var json_file = 'ibc_ip_and_port_ranges.json';
 
 $(document).ready(function() {
-            $("#test").click(function(event){
-                        read_json_file();
-                        console.log(data);
-                 output = '<br>';  
-                 $.each(data, function(key,val){
-                  output += 'Service: ' + val.serviceName + '<br>';
-                  if (typeof val.urls !== "undefined")
-                  {
-                   output += 'URLS: ' + val.urls + '<br>';
-                  }
-                  if (typeof val.ip_ranges !== "undefined")
-                  {            
-                   output += 'IP Addresses: ' + val.ip_ranges + '<br>';
-                  }            
-                  output += 'TCP Ports: ' + val.tcpPorts + '<br>';
-                 });
-                 output += '<br>';
-                 $('#log').html(output);
+            $("#test").click(function(event){                                     
+                         $.getJSON(json_file,function(data){
+                                     console.log(data);
+                                     output = '<br>';  
+                                     $.each(data, function(key,val){
+                                                 output += 'Service: ' + val.serviceName + '<br>';
+                                                 if (typeof val.urls !== "undefined") {
+                                                             output += 'URLS: ' + val.urls + '<br>';
+                                                 }
+                                                 if (typeof val.ip_ranges !== "undefined") {            
+                                                             output += 'IP Addresses: ' + val.ip_ranges + '<br>';
+                                                 }            
+                                                 output += 'TCP Ports: ' + val.tcpPorts + '<br>';
+                                                 output += '<br>';
+                                     });
+                                     
+                                     $('#log').html(output);
+                         });
             });
 });
